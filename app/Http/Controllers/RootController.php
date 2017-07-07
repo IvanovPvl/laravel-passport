674d566c211b893cb1b4e9d3bc48475ea3c80c01
Pavel Ivanov <p.ivanov@vsemayki.ru>
2017-07-07 09:53:02 +0700

    Add RootController.

---
+++ app/Http/Controllers/RootController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+/**
+ * Class RootController
+ * @package App\Http\Controllers
+ */
+class RootController extends Controller
+{
+    public function index()
+    {
+    }
+}
